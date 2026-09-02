5aaa459a169936fd3e304ef9d4e3a895d2d507ad
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-02 11:44:10 +0530

    Added config file to set max limit retry to 3 and 1 sec retry time in between retry

---
+++ Phase_5_Deal_letter_Topic/consumer/src/main/java/com/rahul/consumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,61 @@
+package com.rahul.consumer.config;
+
+import com.rahul.consumer.model.PaymentEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> dltProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> dltKafkaTemplate(ProducerFactory<String, Object> dltProducerFactory) {
+        return new KafkaTemplate<>(dltProducerFactory);
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> dltKafkaTemplate) {
+
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(dltKafkaTemplate,
+                (record, exception) -> new TopicPartition(record.topic() + ".DLT", record.partition()));
+
+        FixedBackOff backOff = new FixedBackOff(1000L, 3L);
+
+        return new DefaultErrorHandler(recoverer, backOff);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> kafkaListenerContainerFactory(
+            ConsumerFactory<String, PaymentEvent> consumerFactory,
+            DefaultErrorHandler errorHandler) {
+
+        ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
+        return factory;
+    }
+}
--- Phase_5_Deal_letter_Topic/producer/src/main/java/com/rahul/producer/controller/PaymentController.java
+++ Phase_5_Deal_letter_Topic/producer/src/main/java/com/rahul/producer/controller/PaymentController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import com.rahul.producer.model.PaymentEvent;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,11 +16,22 @@ public class PaymentController {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+//    @PostMapping("/pay")
+//    public String pay() {
+//        String orderId = UUID.randomUUID().toString();
+//        PaymentEvent event = new PaymentEvent(orderId, 500, "SUCCESS");
+//        kafkaTemplate.send("payments", orderId, event);
+//        return "Payment event sent for order " + orderId;
+//    }
+
+    /*
+    * POST http://localhost:8081/pay → normal payment (defaults to 500)
+    POST http://localhost:8081/pay?amount=999 ->when user enter amount*/
     @PostMapping("/pay")
-    public String pay() {
+    public String pay(@RequestParam(defaultValue = "500") double amount) {
         String orderId = UUID.randomUUID().toString();
-        PaymentEvent event = new PaymentEvent(orderId, 500, "SUCCESS");
+        PaymentEvent event = new PaymentEvent(orderId, amount, "SUCCESS");
         kafkaTemplate.send("payments", orderId, event);
-        return "Payment event sent for order " + orderId;
+        return "Payment event sent for order " + orderId + " with amount " + amount;
     }
 }
